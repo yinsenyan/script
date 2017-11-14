484d31549400179074c085eecfc7ceb1711409e6
01deyishu <01deyishu@gmail.com>
2017-11-14 23:40:59 +0800

    debug ^M

--- irq.py
+++ irq.py
@@ -1,1 +1,17 @@
-import osimport sysimport psutildef getNIC():	nic = psutil.net_if_addrs()	for i in nic.items():		print i[0]def getCPU():	cpu = psutil.cpu_percent()	print cpu	#for i in cpu.index():	#	print i if __name__ == '__main__':	getNIC()	getCPU()
+import os
+import sys
+import psutil
+
+def getNIC():
+	nic = psutil.net_if_addrs()
+	for i in nic.items():
+		print i[0]
+
+def getCPU():
+	cpu = psutil.cpu_percent()
+	print cpu
+
+if __name__ == '__main__':
+	getNIC()
+	getCPU()
+
