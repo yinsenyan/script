43df2cf80da1141d92a3f57f663b9567dfe4f6af
01deyishu <01deyishu@gmail.com>
2018-03-06 19:27:37 +0800

    添加一些数据结构，当前版本有bug，明天调试

--- dnstool.go
+++ dnstool.go
@@ -1,41 +1,76 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 )
 
-type dnsServer struct {
-	url    string
-	token  string
-	format string
+type post struct {
+	url       string
+	token     string
+	format    string
+	domain_id string
+	record_id string
+}
+
+type domain struct {
+	Status status   `json:"status"`
+	Domain string   `json:"domain"`
+	Info   string   `json:"info"`
+	Record []record `json:"records"`
+}
+
+type status struct {
+	Code       string `json:"code"`
+	Message    string `json:"message"`
+	Created_at string `json:"created_at"`
 }
 
 type record struct {
-	domain      string
-	record_id   int
-	sub_domain  string
-	record_type string
-	record_line string
-	value       string
-	status      string
-	format      string
+	Id          string `json:“id”`
+	Ttl         string `json:"ttl"`
+	Value       string `json:"value"`
+	Status      string `json:"status`
+	Updated_on  string `json:"updated_on"`
+	Name        string `json:"name"`
+	Line        string `json:"line"`
+	Record_type string `json:"type"`
+}
+
+type token struct {
+	Url       string `json:"url"`
+	Token     string `json:"token"`
+	Format    string `json:"format"`
+	Domain_id string `json:"domain_id"`
+	Record_id string `json:"record_id"`
 }
 
 func main() {
-	url := "https://dnsapi.cn/Record.List"
-	fmt.Println("Current domain record list")
-	getRecordList(url)
+	t := unmarshalToken()
+	getRecordList(t)
 }
 
-func getRecordList(url string) {
-	r, err := http.Post(url, "application/x-www-form-urlencoded", strings.NewReader(token))
+func unmarshalToken() (t token) {
+	tokenFile, err := ioutil.ReadFile("token.json")
+	if err != nil {
+		fmt.Println("error open file token.json")
+	}
+	json.Unmarshal(tokenFile, &t)
+	return
+}
+
+func getRecordList(t token) {
+	var d domain
+	r, err := http.Post(t.Url, "application/x-www-form-urlencoded", strings.NewReader(t.Token+"&"+t.Format+"&"+t.Domain_id+"&"+t.Record_id))
 	if err != nil {
 		fmt.Println("Get domain list err")
+		fmt.Println(err.Error())
 	}
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
-	fmt.Println(string(body))
+	json.Unmarshal(body, &d)
+	fmt.Println(d)
 }
