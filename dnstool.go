051e03f48b2c06b330ff04800c7ec00cf6e2c9a4
01deyishu <01deyishu@gmail.com>
2018-03-25 23:01:06 +0800

    更新

--- dnstool.go
+++ dnstool.go
@@ -1,9 +1,18 @@
 //需求：
 //0. 通过参数指定集群、机器、线路
-//1. 停止某急群中某台服务器的IP的解析
-//2. 停止某集群中某个运营商的IP的解析
+//1. 停止某集群中某个运营商的IP解析
+//2. 恢复某急群众某个运营商的IP解析
 //3. 如果2中停止的是默认运营商，则选定另一个运营商修改为默认
 //4. 恢复到预期的解析状态
+
+//API列表
+//记录列表:https://dnsapi.cn/Record.List
+//	curl -X POST https://dnsapi.cn/Record.List -d 'login_token=LOGIN_TOKEN&format=json&domain_id=2317346'
+//修改记录:https://dnsapi.cn/Record.Modify
+//	curl -X POST https://dnsapi.cn/Record.Modify -d 'login_token=LOGIN_TOKEN&format=json&domain_id=2317346&record_id=16894439&sub_domain=www&value=3.2.2.2&record_type=A&record_line_id=10%3D3'
+//修改状态:https://dnsapi.cn/Record.Status
+//	curl -X POST https://dnsapi.cn/Record.Status -d 'login_token=LOGIN_TOKEN&format=json&domain_id=2317346&record_id=16894439&status=disable'
+
 package main
 
 import (
@@ -11,6 +20,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
@@ -22,10 +32,10 @@ type post struct {
 	record_id string
 }
 
-type response struct {
+type allinfo struct {
 	Status status   `json:"status"`
 	Domain domain   `json:"domain"`
-	Info   string   `json:"info"`
+	Info   info     `json:"info"`
 	Record []record `json:"records"`
 }
 
@@ -42,6 +52,11 @@ type domain struct {
 	Status string  `json:"status"`
 }
 
+type info struct {
+	Sub_domains  string `json:"sub_domains"`
+	Record_total string `json:"record_total"`
+}
+
 type record struct {
 	Id          string `json:“id”`
 	Ttl         string `json:"ttl"`
@@ -53,37 +68,67 @@ type record struct {
 	Record_type string `json:"type"`
 }
 
-type token struct {
-	Url       string `json:"url"`
+type api struct {
+	Rl        string `json:"recordlist"`
+	Rm        string `json:"recordmodify"`
+	Rs        string `json:"recordstatus"`
 	Token     string `json:"token"`
 	Format    string `json:"format"`
 	Domain_id string `json:"domain_id"`
-	Record_id string `json:"record_id"`
 }
 
-func main() {
-	t := unmarshalToken()
-	getRecordList(t)
-}
-
-func unmarshalToken() (t token) {
-	tokenFile, err := ioutil.ReadFile("token.json")
+func unmarshalApi() (a api) {
+	apifile, err := ioutil.ReadFile("api.json")
 	if err != nil {
-		fmt.Println("error open file token.json")
+		fmt.Println("error open file api.json")
 	}
-	json.Unmarshal(tokenFile, &t)
+	json.Unmarshal(apifile, &a)
 	return
 }
 
-func getRecordList(t token) {
-	var info response
-	api := t.Token + "&format=" + t.Format + "&domain_id=" + t.Domain_id //+ "&record_id=" + t.Record_id
-	r, err := http.Post(t.Url, "application/x-www-form-urlencoded", strings.NewReader(api))
+func getRecordList(a api) (response allinfo) {
+	postdate := a.Token + "&format=" + a.Format + "&domain_id=" + a.Domain_id //+ "&record_id=" + a.Record_id
+	r, err := http.Post(a.Rl, "application/x-www-form-urlencoded", strings.NewReader(postdate))
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(a.Rl)
+		fmt.Println("http request err : ", err.Error())
 	}
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
-	json.Unmarshal(body, &info)
-	fmt.Println(info.Domain.Status)
+	if err != nil {
+		fmt.Println("get domain response body error")
+	}
+	json.Unmarshal(body, &response)
+	return
 }
+
+func main() {
+	a := unmarshalApi()
+	res := getRecordList(a)
+	fmt.Println(res.Record)
+	//changeRecordStatus("test", "test", "disable", res, a)
+}
+
+func changeRecordStatus(cluster, isp, record_status string, res allinfo, a api) (response allinfo) {
+	for i := 0; i < len(res.Record); i++ {
+		if b, _ := regexp.MatchString(cluster, res.Record[i].Name); b {
+			postdate := a.Token + "&format=" + a.Format + "&domain_id=" + a.Domain_id + "&record_id=" + res.Record[i].Id + "&status=" + record_status
+			r, err := http.Post(a.Rs, "application/x-www-form-urlencoded", strings.NewReader(postdate))
+			if err != nil {
+				fmt.Println("http request err : ", err.Error())
+			}
+			defer r.Body.Close()
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Println("get domain response body error")
+			}
+			json.Unmarshal(body, &response)
+			fmt.Println(response.Status)
+		}
+	}
+	return
+}
+
+// func (*record) modifyRecord(api string) {
+
+// }
